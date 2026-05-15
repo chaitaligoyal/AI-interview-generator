ca1f071365728ecf0d26d787b3781d95ebb333ac
Chaitali Goyal <chaitaligoyal@Chaitalis-MacBook-Air.local>
2026-05-15 15:28:32 +0530

    Added resume feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from openai import OpenAI
 from dotenv import load_dotenv
+from pypdf import PDfReader
 import os
 
 load_dotenv()
@@ -20,16 +21,68 @@ level = st.selectbox(
     ["Beginner", "Intermediate", "Advanced"]
 )
 
+uploaded_file = st.file_uploader(
+    "Upload Resume File(PDF)",
+    type = ["pdf"]
+)
+
+resume_text = ""
+
+if uploaded_file is not None:
+    pdf_reader = PDfReader(uploaded_file)
+
+    for page in pdf_reader.pages:
+        resume_text += page.extract_text()
+
 if st.button("Generate Questions"):
 
+    if uploaded_file is None:
+        st.warning("Please upload a resume.")
+
     prompt = f"""
-    Generate interview questions for:
+    You are an expert technical interviewer.
 
-    Role: {role}
+    Review the resume and generate:
+
+    1.Technical interview Questions and Answers
+    2.HR interview Questions and Answers
+
+    Also categorize technical and HR questions separately.
+    Give proper spacing and formatting.
+
+    Candidate Role: {role}
     Skills: {skills}
-    Level: {level}
+    Experience Level: {level}
 
-    Also provide medium answers.
+    Resume Content: {resume_text}
+
+    """
+
+if st.button("Review my Resume"):
+
+    if uploaded_file is None:
+    st.warning("Please upload a resume.")
+
+    prompt = f"""
+    As an expert technical interviewer.
+
+    Review the resume and generate:
+
+    1.Candidate strengths
+    2.Potential additions to resume to make it better
+    3.Resume score (1 to 10) with 10 being the best quality resume
+
+    Candidate Role: {role}
+    Skills: {skills}
+    Experience Level: {level}
+
+    Resume Content: {resume_text}
+
+    Format the response using:
+    - clear headings
+    - bullet points
+    - proper spacing
+
     """
 
     try:
